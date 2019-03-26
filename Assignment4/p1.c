5c87607dfe28f7b13ff7b3fbf2b5f2abe44b2d25
DouglasGuerra <douaguerra@gmail.com>
2019-03-25 21:59:41 -0400

    Added longer string size and words in string, modified code to reverese string; Tested and working

--- Assignment4/p1.c
+++ Assignment4/p1.c
@@ -7,45 +7,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define DEFAULT_STRING_LENGTH 255
-#define DEFAULT_WORDS_IN_A_STRING 100
+#define DEFAULT_STRING_LENGTH 1024
+#define DEFAULT_WORDS_IN_A_STRING 2048
 
-void EmptyOutStrings(char *string, int size){
-    int index;
-    for(index = 0; index < size; index++)
-        string[index] = 0;
+char wordsInString[DEFAULT_WORDS_IN_A_STRING][DEFAULT_STRING_LENGTH];
+int wordsStoreIndex = 0;
+
+void StoreAndClearString(char words[][DEFAULT_STRING_LENGTH], char string[], int stringSize){
+    if(stringSize > 0){
+        int i;
+        for(i = 0; i < stringSize; i++){
+            words[wordsStoreIndex][i] = string[i];
+            string[i] = 0;
+        }
+
+        wordsStoreIndex++;
+    }
 }
 
 int main(){
 
     char readInChar;
-    char string[DEFAULT_STRING_LENGTH];
-    char wordsInString[DEFAULT_WORDS_IN_A_STRING][DEFAULT_STRING_LENGTH];
-    int stringSize = 0, wordsStored = 0;
+    char string[DEFAULT_STRING_LENGTH] = { 0 };
+    char revString[DEFAULT_STRING_LENGTH] = { 0 };
+    int stringSize = 0;
 
     // Reading in line and storing them in an array
-    EmptyOutStrings(string, DEFAULT_STRING_LENGTH);
     scanf("%c", &readInChar);
     while(readInChar != '\n'){
         if(readInChar == ' '){
-            sprintf(wordsInString[wordsStored++], "%s", string);
-            EmptyOutStrings(string, stringSize);
+            StoreAndClearString(wordsInString, string, stringSize);
             stringSize = 0;
         }
         else{
             string[stringSize++] = readInChar;
         }
+
         scanf("%c", &readInChar);
     }
-    if(stringSize > 0) sprintf(wordsInString[wordsStored++], "%s", string);
-    EmptyOutStrings(string, DEFAULT_STRING_LENGTH);
+    StoreAndClearString(wordsInString, string, stringSize);
 
-    // Reversing the words in the string
-    int index = 0;
-    sprintf(string, "%s", wordsInString[wordsStored-1]);
-    for(index = wordsStored-2; index >= 0; index--)
-        sprintf(string, "%s %s", string, wordsInString[index]);
+    // Print out words in reverse order
+    if(wordsStoreIndex > 0){
+        int i;
+        for(i = wordsStoreIndex-1; i >= 0; i--){
+            printf("%s", wordsInString[i]);
+            if(i > 0) printf(" ");
+        }
+        printf("\n");
+    }
 
-    printf("%s\n", string);
     return 0;
 }
